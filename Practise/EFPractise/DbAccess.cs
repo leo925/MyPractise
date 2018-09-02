727a7ffff0907132e57f3b1595ead60a4a212d06
leoliu <leoliu925@163.com>
2018-09-02 23:26:07 +0800

    9.2

--- Practise/EFPractise/DbAccess.cs
+++ Practise/EFPractise/DbAccess.cs
@@ -27,11 +27,26 @@ namespace EFPractise
         {
             using (var context = new MyPractiseDBEntities())
             {
-                var allReaders = context.ReaderModels.Take(20).ToList();
+               // context.Configuration.ValidateOnSaveEnabled = false;
+
+                var allReaders = context.ReaderModels.Take(30).ToList();
                 foreach (var reader in allReaders)
                 {
                     Console.WriteLine(reader.ReaderName);
+
                 }
+
+
+                
+                var reader3 = context.ReaderModels.Find(1004);
+                reader3.ReaderName = "good reade666";
+
+
+
+                var reader2 = context.ReaderModels.Find(1005);
+                reader2.ReaderName = "good reade2888";
+                context.SaveChanges();
+          
             }
         }
     }
