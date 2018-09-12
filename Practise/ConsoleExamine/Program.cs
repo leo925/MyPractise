cc53d2826669565f6e57a831108913462f13e847
Active\zliu <Zhihang.Liu@activenetwork.com>
2018-09-12 13:41:09 +0800

    no message

--- Practise/ConsoleExamine/Program.cs
+++ Practise/ConsoleExamine/Program.cs
@@ -10,14 +10,63 @@ namespace ConsoleExamine
 {
     class Program
     {
+        static Mutex m;
         static void Main(string[] args)
         {
 
-         
 
+
+            TestEvent();
             Console.Read();
+             
+
         }
 
+        static void TestEvent()
+        {
+
+            ManualResetEventSlim manual = new ManualResetEventSlim(false);
+            var waithandle = manual.WaitHandle;
+
+            Task t = new Task((obj) =>
+            {
+                Console.WriteLine("start to work");
+                Thread.Sleep(2000);
+                var m = obj as ManualResetEventSlim;
+                m.Set();
+                m.Reset();
+            }, manual);
+            t.Start();
+            while (waithandle.WaitOne(100) == false)
+            {
+                Console.WriteLine("waint for event");
+                manual.Reset();
+            }
+            Console.WriteLine("complete waiting");
+        }
+
+        private static void TestMutex()
+        {
+            bool isNew;
+            m = new Mutex(true, "leo", out isNew);
+            if (!isNew)
+            {
+                try
+                {
+                    while (!m.WaitOne(2000))
+                    {
+                        Console.WriteLine("I am waitning");
+                    }
+                    Console.WriteLine("haha, I got the lock");
+                }
+                catch (Exception ex)
+                {
+
+                    var str = ex.Message;
+                }
+             
+            }
+        }
 
         public static void Test()
         {
