f2ac8e46fe0ebda70429bb9574fdc8c070878a75
leoliu <leoliu925@163.com>
2018-12-03 20:38:48 +0800

    no message

---
+++ Practise/SilverlightPractise/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightPractise
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            App.Current.RootVisual = (Page)Shell;
+           
+        }
+    }
+}
