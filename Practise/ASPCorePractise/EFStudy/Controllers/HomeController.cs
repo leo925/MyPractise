6d29eaca26d0e309b8b50d89d625a3af6db29fa7
leoliu <leoliu925@163.com>
2020-02-05 23:31:15 +0800

    no message

---
+++ Practise/ASPCorePractise/EFStudy/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFStudy.Models;
+using EFStudy.DataBaseContext;
+
+namespace EFStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFDatabaseContext context;
+        public HomeController(EFDatabaseContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var allProducts = context.Producs;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practise/ASPCorePractise/EFStudy/DataBaseContext/EFDatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFStudy.DataBaseContext
+{
+    public class EFDatabaseContext:DbContext
+    {
+        public EFDatabaseContext(DbContextOptions<EFDatabaseContext> opts)
+            : base(opts) { }
+
+        public DbSet<Product> Producs { get; set; }
+
+    }
+}
