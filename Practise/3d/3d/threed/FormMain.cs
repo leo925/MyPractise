6376b7594980281647864ce6910103e35cab5055
leoliu <leoliu925@163.com>
2020-04-05 21:09:43 +0800

    no message

--- Practise/3d/3d/threed/FormMain.cs
+++ Practise/3d/3d/threed/FormMain.cs
@@ -10,9 +10,9 @@ using threed.DataManage;
 
 namespace threed
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
