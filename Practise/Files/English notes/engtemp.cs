e8909424e08bf2135bc52f91ac174a865174304b
leoliu <leoliu925@163.com>
2020-04-05 18:21:36 +0800

    no message

--- Practise/Files/English notes/engtemp.cs
+++ Practise/Files/English notes/engtemp.cs
@@ -3,7 +3,8 @@
  cut out the middleman and do ourselves; drop shipping
  as far as sth is concerned:就某事而言  Simon is doing fine as far as english is concerned
  If a company launches a new product, it makes it available to the public. 推出 (新产品)/发起
- When you talk about the twenties, you are referring to numbers between 20 and 29. For example, if you are in your twenties, you are aged between 20 and 29.
+ When you talk about the twenties, you are referring to numbers between 20 and 29. 
+ For example, if you are in your twenties, you are aged between 20 and 29.
  --If the temperature is in the twenties, the temperature is between 20 and 29 degrees. 二十几
  They're both in their twenties / her early thirties
 
