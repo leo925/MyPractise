36a651df7ea6aff5ab60068d79c98e6b802d3875
leoliu <leoliu925@163.com>
2020-10-18 16:18:30 +0800

    prism- install prism, add shell, inject ModuleA

---
+++ Practise/ModuleA/ModuleAModule.cs
@@ -0,0 +1,30 @@
+﻿using ModuleA.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+        public ModuleAModule(IRegionManager regionManager) {
+            _regionManager = regionManager;
+        }
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof
+                (ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Practise/WpfPractise/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ModuleA;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Windows;
+using WpfPractise.Views;
+
+namespace WpfPractise
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            //DI container in this case dry IOC used to resolve the instance
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleAModule>( InitializationMode.WhenAvailable);
+        }
+
+    }
+}
