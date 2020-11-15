877de117ae95ed4039256bfbc1bdc416e20be5dc
leoliu <leoliu925@163.com>
2020-11-15 12:54:35 +0800

    no message

--- Practise/AppData/Models/ZoomShoesDbContext.cs
+++ Practise/AppData/Models/ZoomShoesDbContext.cs
@@ -26,7 +26,7 @@ namespace AppData.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ZoomShoesDb;Integrated Security=True;");
             }
         }
