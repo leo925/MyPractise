3cf1eeca4cf11a340af317f6e3e4d45215f5c238
leoliu <leoliu925@163.com>
2020-11-22 16:05:00 +0800

    no message

--- Practise/BootstrapPractise/Controllers/HomeController.cs
+++ Practise/BootstrapPractise/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace BootstrapPractise.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult List()
         {
             return View();
         }
--- Practise/BootstrapPractise/Startup.cs
+++ Practise/BootstrapPractise/Startup.cs
@@ -53,7 +53,7 @@ namespace BootstrapPractise
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=List}/{id?}");
             });
         }
     }
