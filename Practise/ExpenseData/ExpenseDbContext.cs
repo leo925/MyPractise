0e4ff0ba1181ac7decf82056cd1990670d5b1e34
leoliu <leoliu925@163.com>
2021-04-21 14:21:36 +0800

    20210421

--- Practise/ExpenseData/ExpenseDbContext.cs
+++ Practise/ExpenseData/ExpenseDbContext.cs
@@ -6,9 +6,15 @@ namespace ExpenseData
 {
     public class ExpenseDbContext :DbContext
     {
+        private readonly string connectionStr = @"Data Source=MYDESKTOP;Initial Catalog=Expense;User ID=leo;Password=123456;" +
+            "Connect Timeout=30;";//Application Name = Expense App;
         public DbSet<Expense> Expenses { get; set; }
 
         public DbSet<Spender> Spenders { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionStr);
+        }
     }
 }
